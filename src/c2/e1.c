0994d8b7b8c14e89a7ce14297afec25a753975f4
Thiago Macedo <119901046+o-thiago@users.noreply.github.com>
2026-04-19 10:34:19 -0400

    Simplify macro on exercise 1

---
+++ src/c2/e1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define LIMITS(T, fmt_s, fmt_u)                                      \
+    {                                                                \
+        unsigned T u = 0;                                            \
+        u--; /* unsigned wrap → max value */                         \
+                                                                     \
+        T max = (T)(u >> 1); /* clear sign bit -> max signed value*/ \
+        T min = (T)(-max - 1);                                       \
+                                                                     \
+        printf(#T " signed: " fmt_s " to " fmt_s "\n", min, max);    \
+        printf("unsigned " #T ": 0 to " fmt_u "\n\n", u);            \
+    }
+
+int main(void) {
+    LIMITS(char, "%d", "%u");
+    LIMITS(short, "%d", "%u");
+    LIMITS(int, "%d", "%u");
+    LIMITS(long, "%ld", "%lu");
+    return 0;
+}
